81810f804bde180d27c39e8e3f00f327f5a01352
wygol <124050566+wygol@users.noreply.github.com>
2025-03-16 10:32:45 +0100

    Create informationAboutMe.py
    added a python file with information about myself

---
+++ informationAboutMe.py
@@ -0,0 +1,22 @@
+def leastFavoriteFood() -> str:
+    return "gummy bears\n---"
+
+
+def recommendation() -> str:
+    return "Book: Harvard Medical School Guide (it's a book about proper nutrition)\n---"
+  
+
+def quote() -> str:
+    return "happiness is a decision. not a state. - someone\n---"
+
+
+def germanWord() -> str:
+    return "Freifahrtschein - means that you can do something for free i.e. a free ticket\n---"
+
+
+
+if __name__ == "__main__":
+    print(leastFavoriteFood())
+    print(recommendation())
+    print(quote())
+    print(germanWord())
